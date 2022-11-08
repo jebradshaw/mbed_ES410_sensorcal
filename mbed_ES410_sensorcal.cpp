44f96b541c494f10341c0afbfe62faab32c7b32f
jebradshaw <bradshaw@usna.edu>
2022-11-08 13:51:43 -0500

    Add files via upload

---
+++ mbed_ES410_sensorcal.cpp
@@ -0,0 +1,11 @@
+#include "mbed.h"
+#include "QEI.h"
+
+QEI enc1(p24,p23,NC,800, QEI::X4_ENCODING); //Global Motor 800 cpr w x4 encoding
+
+int main() {
+    while(1) {
+        int enc = enc1.getPulses(); //reads the current number of encoder pulses
+        printf("%d  \r", enc);      //prints to the USB serial port (Default is 9600 baud);
+    }
+}
